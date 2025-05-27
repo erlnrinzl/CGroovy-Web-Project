551f7b800dba064e293db320e167a1ac54e49e9a
erlnrinzl <henrichfergian@gmail.com>
2025-05-28 01:55:48 +0700

    refractor js code that used globally

--- script.js
+++ script.js
@@ -82,19 +82,5 @@ $(function () {
     $(window).on('resize', function () {
         updateSliderStyles();
     });
-
-    function toggleMobileNav() {
-        $('.mobile-nav-links').toggleClass('active');
-    }
-
-    $('#mobile-menu-btn').on('click', function () {
-        toggleMobileNav();
-    });
-
-    $(document).on('click', function(e) {
-        if (!$(e.target).closest('.mobile-nav-links, #mobile-menu-btn').length) {
-            $('.mobile-nav-links').removeClass('active');
-        }
-    });
     
 });
