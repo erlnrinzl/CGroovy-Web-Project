44377b6c0bde61017d404d0bf2bb5be260c45dcf
erlnrinzl <henrichfergian@gmail.com>
2025-06-01 12:47:32 +0700

    refractor navbar menu into dropdown css style

--- global.js
+++ global.js
@@ -1,16 +1,3 @@
 $(function () { 
-    function toggleMobileNav() {
-        $('.mobile-nav-links').toggleClass('active');
-    }
-
-    $('#mobile-menu-btn').on('click', function () {
-        toggleMobileNav();
-    });
-
-    $(document).on('click', function(e) {
-        if (!$(e.target).closest('.mobile-nav-links, #mobile-menu-btn').length) {
-            $('.mobile-nav-links').removeClass('active');
-        }
-    });
     
 });
